c87ce468ea205814669abb9a59357c0efd1bcbc6
Diego Bas <dbas@gespa.local>
2022-09-28 14:17:48 +0200

    Allow escape character backslash in date format

--- src/Converter.php
+++ src/Converter.php
@@ -15,11 +15,25 @@ class Converter
     {
         $format = '';
 
+        $escape = false;
         foreach (str_split($this->format) as $token) {
+            if ($escape) {
+                $format .= "[{$token}]";
+                $escape = false;
+                continue;
+            }
+
+            if ($token === '\\') {
+                $escape = true;
+                continue;
+            }
+
             $format .= array_key_exists($token, DATE_FORMAT_STANDARDS) ?
                 DATE_FORMAT_STANDARDS[$token][$standard] :
                 $token;
         }
+
+        $format = str_replace('][', '', $format);
 
         return $format;
     }
